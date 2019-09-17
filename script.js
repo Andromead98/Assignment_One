de36e20a820dbda00a332b9c57cfe75305c9770b
Ann B <48338402+Andromead98@users.noreply.github.com>
2019-09-17 10:01:17 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+//list tag stuff
+var myList = document.getElementsByTagName('li');
+var index;
+for (index = 0; index < myList.length; index++){
+  var aSpanTag = document.createElement("SPAN");
+  var someTxt = document.createTextNode("\u00D7");
+  aSpanTag.className = "close";
+  aSpanTag.appendChild(someTxt);
+  myList[index].appendChild(aSpanTag);
+}
+//creating close button for lists not created with main function
+
+var closeButton = document.getElementsByClassName("close");
+
+for (i = 0; i < closeButton.length; i++){
+  closeButton[i].onclick = function(){
+    var theDiv = this.parentElement;
+    theDiv.style.display = "none";
+  }
+}
+//
+var ulList = document.querySelector('ul');
+ulList.addEventListener('click', function(event){
+  console.log(event);
+  if(event.target.tagName === "LI"){
+    event.target.classList.toggle('checked');
+  }
+}, false);
+
+//
+
+function createNewElement(){
+  var li = document.createElement('li');
+  var theInputValue = document.getElementById("the-input").value;
+  var textNode = document.createTextNode(theInputValue);
+  li.appendChild(textNode);
+
+  if (theInputValue === ''){
+    alert("This Field can not be left empty")
+  } else {
+    document.getElementById("the-ul").appendChild(li);
+  }
+
+  document.getElementById("the-input").value = "";
+
+  var thePanTag = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  thePanTag.className = "close";
+  thePanTag.appendChild(txt);
+  li.appendChild(thePanTag);
+
+  for (i = 0; i < closeButton.length; i++){
+        closeButton[i].onclick = function(){
+            var theDiv = this.parentElement;
+            theDiv.style.display = "none";
+        }
+    }
+}
