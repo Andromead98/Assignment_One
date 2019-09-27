c3967f9a99a766eddad6cc11e5b277a2f60c8338
andromead98 <a.burned98@gmail.com>
2019-09-27 11:01:22 +0930

    reverted back to old changes to work on removing onclick events before working on local storage

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//adding new lists & local storage
+//https://www.youtube.com/watch?v=W7FaYfuwu70&t=830s
+const ListsContainer = document.querySelector('[data-lists]')
+
+
 
 //hamburger Menu
 function openSlideMenu(){
@@ -9,52 +14,6 @@ function closeSlideMenu(){
   document.getElementById('menu').style.width = '0';
   document.getElementById('list-of-lists').style.marginLeft ='0';
 }
-
-//adding new lists & local storage
-//https://www.youtube.com/watch?v=W7FaYfuwu70&t=830s
-const listsContainer = document.querySelector('[data-lists]')
-const newListForm = document.querySelector('[data-new-list-form]')
-const newListInput = document.querySelector('[data-new-list-input]')
-
-let lists =[{
-  id: 1,
-  name: 'name'
-}, {
-  id: 2,
-  name:'todo'
-}]
-
-newListForm.addEventListener('submit', event =>{
-  event.preventDefault()
-  const listName = newListInput.value
-  if (listName == null || listName ==='') return
-  const list =createList(listName)
-  newListInput.value = null
-})
-
-//function createList(name){
-  //{id: Date.now().toString(), name: name, tasks:[]}
-//}
-
-function render(){
-  clearElement(listsContainer)
-  lists.forEach(list =>{
-    const ListElement = document.createElement('li')
-    listElement.dataset.listId = list.id
-    ListElement.classList.add("otherLists")
-    ListElement.innerText = list.name
-    listsContainer.appendChild(ListElement)
-  })
-}
-
-function clearElement(){
-  while(element.firstChild){
-    element.removeChild(element.firstChild)
-  }
-
-}
-
-render()
 
 //list tag stuff
 var myList = document.getElementsByTagName('li');
@@ -86,6 +45,10 @@ ulList.addEventListener('click', function(event){
 }, false);
 
 //
+const CreateTask = document.getElementsByClassName('create-button');
+for (i=0; i<CreateTask.length; i++)
+  CreateTask[i].onclick= createNewElement;
+
 
 function createNewElement(){
   var li = document.createElement('li');
@@ -114,5 +77,3 @@ function createNewElement(){
         }
     }
 }
-
-//create new lists
