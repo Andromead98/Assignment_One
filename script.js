9e06d0773afa59ccda9d33d60d039dfebe9fa773
andromead98 <a.burned98@gmail.com>
2019-10-01 16:11:54 +0930

    changed name of template to displayTask comment blocks added to explain actions, with help from Matt Elvey(lecturer) const data moved lower in the script added JSON.parse to getItem added in not-Array.isArray so that if any data went bad/corrupted, it would be replaced with an empty array added in function that would purge tasks with array and display them when called. removed code that didnt work.

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const submit = document.querySelector('.create-button');
 const taskList = document.querySelector('.the-ul-list');
 const taskInput = document.querySelector('.the-input-class');
 
-function template(data){
+/**
+* Display data in the list
+* @param {Object} data The item we want to display
+* @param {String} data.task The task text to display
+**/
+function displayTask(data){
   taskList.insertAdjacentHTML("beforeend", `
   <ul id="the-ul" class="the-ul-list">
     <li id="the-li" class="task">${data.task}<span class ="close">x</span></li>
@@ -14,40 +19,55 @@ function template(data){
 
 }
 
-
+/**
+* Event listener to append a task to the list
+* @param {ClickEvent} event ClickEvent when add task is clicked
+**/
 function appendTask(event){
-  const data = {
-    task: taskInput.value,
-  }
-
   event.preventDefault();
 
   //if comment value is smaller than 1, submit will not work
-  if (taskInput.value.length < 1) return;
-
-  template(data);
+  if (taskInput.value.length < 1) {
+    return;
+  }
+//calls for the information
+  const data = {
+    task: taskInput.value,
+  }
+//prints the task at the end of the list
+  displayTask(data);
 
   taskInput.value ="";
 
   // saving the tasks to localStorage
-  localStorage.setItem('taskItems', taskList.innerHTML)
+  let dataList = JSON.parse(localStorage.getItem('taskItems'));
+  if(!dataList || !Array.isArray(dataList)) {
+    dataList = [];
+  }
+  //adds tasks to the array
+  dataList.push(data);
 
+  localStorage.setItem('taskItems', JSON.stringify(dataList));
 }
 
+/**
+* Load task list from local storage and display in list
+**/
+function loadDataList() {
+  const dataList = JSON.parse(localStorage.getItem('taskItems'));
+  if(!dataList || !Array.isArray(dataList)) {
+    return;
+  }
+
+  dataList.forEach(function(item) {
+    displayTask(item);
+  });
+}
+//listens to when the button is clicked so that the task is added to the list
 submit.addEventListener('click', appendTask, false);
+//calls the previous function
+loadDataList();
 
-let saved = localStorage.getItem('taskItems');
-console.log('it worked');
-
-if (!saved) {
-  // taskList.innerHTML = saved;
-  const data = { tasks : taskList.innerHTML }
-  localStorage.setItem('tasksItems',data);
-} else {
-  const data = JSON.parse(saved);
-  saved.push(taskList.innerHTML);
-  localStorage.setItem('taskItems', data);
-}
 
 // removing items from local storage
 
