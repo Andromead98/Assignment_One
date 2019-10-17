44835203881d19a0050de5c7e98d0cd6575d79c4
andromead98 <a.burned98@gmail.com>
2019-10-17 23:24:57 +0930

    added in night mode (css and html) added in class to list template so night mode would be compatible added in transitions to all previous css removed all commented out code (to tidy up the document) removed hamburger menu css

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function displayTask(data){
   const id = data.task.replace(/\W/g, '')
 
   taskList.insertAdjacentHTML("beforeend", `
-    <li id="task${id}" data-task=${id} class="task"><span>${data.task}</span><button class="close" id="btn${id}">x</button></li>
+    <li class="task-item" id="task${id}" data-task=${id} class="task"><span>${data.task}</span><button class="close" id="btn${id}">x</button></li>
   `);
 
   const closeBtn = document.querySelector(`#task${id} button`);
@@ -78,34 +78,6 @@ submit.addEventListener('click', appendTask, false);
 //calls the previous function
 loadDataList();
 
-/* Artifact from earlier button creation. DELETE ME
-var myList = document.getElementsByTagName('li');
-var index;
-for (index = 0; index < myList.length; index++){
-}
-*/
-/*
-// removing items from local storage
-//https://stackoverflow.com/questions/33357900/remove-an-element-from-array-with-user-input
-//https://salesforce.stackexchange.com/questions/144941/how-to-remove-item-from-localstorage
-//close button code
-var closeButton = document.getElementsByClassName("close");
-for (i = 0; i < closeButton.length; i++){
-  console.log('Iterate', i);
-  console.log('Close button', closeButton[i]);
-  closeButton[i].addEventListener('click', function(evt) {
-    console.log('index', i);
-    const target = evt.target;
-    var theDiv = target.parentElement;
-    theDiv.style.display = "none";
-    removeTaskFromStorage(removeTask);
-  });
-}
-*/
-
-//const dataList = JSON.parse(localStorage.getItem('taskItems'));
-//console.log(dataList);
-//
 var removeTaskFromStorage = function(evt){
   evt.preventDefault();
   const clicked = evt.target;
@@ -127,38 +99,6 @@ var removeTaskFromStorage = function(evt){
     location.reload();
   }
 }
-/* dataList.indexOf('{!task.ID}');
-if (removeTask > -1){
-  dataList.splice(removeTask, 1);
-  localStorage['Tasks'] = ;
-}
-
-/*function removeTask(){
-  //call data
-  const dataList = JSON.parse(localStorage.getItem('taskItems'));
-  //check if array is being pulled
-  console.log(dataList);
-  //click event to remove item from Array
-  // looked at this: https://stackoverflow.com/questions/36888969/how-to-delete-an-array-element-stored-in-local-storage
-  // and this: https://developer.mozilla.org/en-US/docs/Web/API/Storage/removeItem
-
-  //this code removes the task from the list, but not the array.
-  //need to add something that removes item from array
-  //
-  var closeButton = document.getElementsByClassName("close");
-
-  for (i = 0; i < closeButton.length; i++){
-    closeButton[i].addEventListener('click', function(){
-      var theDiv = this.parentElement;
-      theDiv.style.display = "none";
-    })
-  }
-
-
-
-}
-*/
-
 
 //text decoration
 var ulList = document.querySelector('ul');
